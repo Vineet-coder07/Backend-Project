7f79da6128b2a4b7d2a0194e1b5380c30cb22d07
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-10-29 22:45:51 +0530

    bug fixed

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,148 +1,148 @@
-// import mongoose, { Schema } from "mongoose";
-// import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
+  // import mongoose, { Schema } from "mongoose";
+  // import bcrypt from "bcrypt";
+  // import jwt from "jsonwebtoken";
 
-// const userSchema = new Schema(
-//   {
-//     username: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       lowercase: true,
-//       trim: true,
-//       index: true
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       lowercase: true,
-//       trim: true,
-//     },
-//     fullname: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//       index: true
-//     },
-//     password: {
-//       type: String,
-//       required: [true, "Password is required"]
-//     },
-//     refreshToken: {
-//       type: String
-//     }
-//   },
-//   { timestamps: true }
-// );
+  // const userSchema = new Schema(
+  //   {
+  //     username: {
+  //       type: String,
+  //       required: true,
+  //       unique: true,
+  //       lowercase: true,
+  //       trim: true,
+  //       index: true
+  //     },
+  //     email: {
+  //       type: String,
+  //       required: true,
+  //       unique: true,
+  //       lowercase: true,
+  //       trim: true,
+  //     },
+  //     fullname: {
+  //       type: String,
+  //       required: true,
+  //       trim: true,
+  //       index: true
+  //     },
+  //     password: {
+  //       type: String,
+  //       required: [true, "Password is required"]
+  //     },
+  //     refreshToken: {
+  //       type: String
+  //     }
+  //   },
+  //   { timestamps: true }
+  // );
 
-// // ✅ Hash password before saving
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
+  // // ✅ Hash password before saving
+  // userSchema.pre("save", async function (next) {
+  //   if (!this.isModified("password")) return next();
+  //   this.password = await bcrypt.hash(this.password, 10);
+  //   next();
+  // });
 
-// // ✅ Compare password
-// userSchema.methods.isPasswordCorrect = async function (password) {
-//   return await bcrypt.compare(password, this.password);
-// };
+  // // ✅ Compare password
+  // userSchema.methods.isPasswordCorrect = async function (password) {
+  //   return await bcrypt.compare(password, this.password);
+  // };
 
-// // ✅ Generate Access Token
-// userSchema.methods.generateAccessToken = function () {
-//   return jwt.sign(
-//     {
-//       _id: this.id,
-//       username: this.username,
-//       fullname: this.fullname,
-//       email: this.email,
-//     },
-//     process.env.ACCESS_TOKEN_SECRET,
-//     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-//   );
-// };
+  // // ✅ Generate Access Token
+  // userSchema.methods.generateAccessToken = function () {
+  //   return jwt.sign(
+  //     {
+  //       _id: this.id,
+  //       username: this.username,
+  //       fullname: this.fullname,
+  //       email: this.email,
+  //     },
+  //     process.env.ACCESS_TOKEN_SECRET,
+  //     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  //   );
+  // };
 
-// // ✅ Generate Refresh Token
-// userSchema.methods.generateRefreshToken = function () {
-//   return jwt.sign(
-//     {
-//       _id: this.id,
-//       username: this.username,
-//       fullname: this.fullname,
-//       email: this.email,
-//     },
-//     process.env.REFRESH_TOKEN_SECRET,
-//     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-//   );
-// };
+  // // ✅ Generate Refresh Token
+  // userSchema.methods.generateRefreshToken = function () {
+  //   return jwt.sign(
+  //     {
+  //       _id: this.id,
+  //       username: this.username,
+  //       fullname: this.fullname,
+  //       email: this.email,
+  //     },
+  //     process.env.REFRESH_TOKEN_SECRET,
+  //     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  //   );
+  // };
 
-// export const User = mongoose.model("User", userSchema);
+  // export const User = mongoose.model("User", userSchema);
 
 
-import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+  import mongoose, { Schema } from "mongoose";
+  import bcrypt from "bcrypt";
+  import jwt from "jsonwebtoken";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-  fullname: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true });
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-userSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
-
-userSchema.methods.generateAccessToken = function () {
-  return jwt.sign(
-    {
-      _id: this.id,
-      username: this.username,
-      fullname: this.fullname,
-      email: this.email
+  const userSchema = new Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
     },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-  );
-};
-
-userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign(
-    {
-      _id: this.id,
-      username: this.username,
-      fullname: this.fullname,
-      email: this.email
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
     },
-    process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-  );
-};
+    fullname: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  }, { timestamps: true });
 
-export const User = mongoose.model("User", userSchema);
+  userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  });
+
+  userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+  userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+      {
+        _id: this.id,
+        username: this.username,
+        fullname: this.fullname,
+        email: this.email
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+  };
+
+  userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+      {
+        _id: this.id,
+        username: this.username,
+        fullname: this.fullname,
+        email: this.email
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+  };
+
+  export const User = mongoose.model("User", userSchema);
