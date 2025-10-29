37ec162f317cf1ce5062ff2efc2d47b12a9cd5d4
unknown <rajpalvineet800@gmail.com>
2025-10-29 14:16:20 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { upload } from "./middlewares/multer.middleware.js";
 const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
-app.use(upload.fields([
-    { name: "avatar", maxCount: 1 },
-    { name: "coverImage", maxCount: 1 }
-])); 
-
 // ✅ IMPORTANT: Parser limits BEFORE routes
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
