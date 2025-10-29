6ae9359dc21b494d0a72dbb6ec0b5772ab0cef88
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-10-29 17:16:42 +0530

    req-files fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,18 +1,29 @@
+
 // import { Router } from "express";
 // import { registeruser } from "../controllers/user.controller.js";
+// import { upload } from "../middlewares/multer.middleware.js";
 
 // const router = Router();
-// router.route("/register").post(registeruser);
+
+// // ✅ Multer integrated route
+// router.post("/register", upload.single("avatar"), registeruser);
 
 // export default router;
 
-import { Router } from "express";
+
+import express from "express";
 import { registeruser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
-const router = Router();
+const router = express.Router();
 
-// ✅ Multer integrated route
-router.post("/register", upload.single("avatar"), registeruser);
+router.post(
+  "/register",
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 }
+  ]),
+  registeruser
+);
 
 export default router;
